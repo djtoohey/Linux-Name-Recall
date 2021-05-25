725bed0ba5335cb6f1b9b3d5bd0009f19548bd88
djtoohey <djtoohey@gmail.com>
2021-05-25 20:36:53 +0930

    init

---
+++ echo.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# basic name recall
+# echo -e "Declan\nToohey"
+
+# asking for user name, then recalling name using name var
+echo "what is your name?"
+read name
+echo -e "Hello $name!"
